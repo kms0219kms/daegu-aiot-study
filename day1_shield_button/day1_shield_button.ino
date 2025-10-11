945382cafa4199b323859f3f0580b01ab67d5c32
kms0219kms <67222970+kms0219kms@users.noreply.github.com>
2025-10-11 12:26:45 +0900

    Update example

--- day1_shield_button/day1_shield_button.ino
+++ day1_shield_button/day1_shield_button.ino
@@ -9,24 +9,24 @@
 */
 
 // CONSTANTS
-const int BUTTON_PIN = 2;
-const int LED_PIN = 13;
+const int SW1_PIN = 2;
+const int LED1_PIN = 13;
 
 // VARIABLES
 int buttonState = 0;  // 버튼의 현재 상태 (0: UP, 1: DOWN(Pressed))
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(SW1_PIN, INPUT);
 }
 
 void loop() {
   // 현재 버튼이 눌렸는지 Check
-  buttonState = digitalRead(BUTTON_PIN);
+  buttonState = digitalRead(SW1_PIN);
 
   // (최적화) 어짜피 buttonState가 int이고 LOW/HIGH값을 가지므로 if문 축약함
-  // digitalWrite(LED_PIN, buttonState);
+  // digitalWrite(LED1_PIN, buttonState);
 
   // !! 아두이노는 풀업 (HIGH, LOW가 반대로 되므로 NOT 연산으로 처리)
-  digitalWrite(LED_PIN, !buttonState);
+  digitalWrite(LED1_PIN, !buttonState);
 }
